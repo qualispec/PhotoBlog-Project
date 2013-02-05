1b7c0d53d5eff5f48e3bb76ec0676605efa91510
Vincent & Ryan <vincent+ryan@appacademy.io>
2013-02-05 09:29:17 -0800

    Initial commit with bundle installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoBlog::Application.config.secret_token = '37c55ccf7f8c15a9a371469e3358ec4c138359c2ddb2f54df9dd237c317cf0e5172092db4c6dd5259dfabdd0675845867141aff564317b383632b926926d8d75'
